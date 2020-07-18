6a72041061e2df968a2a535298a12a3e962c4948
YusKom <yuskom10@gmail.com>
2020-07-18 15:44:25 +0500

    Updates

---
+++ precache-manifest.9f44885fe2268f868833819de7347883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215fc2c74ba832974da3649d66008672",
+    "url": "/waysamurai/index.html"
+  },
+  {
+    "revision": "06d6e623bf248a05b00f",
+    "url": "/waysamurai/static/css/main.3791b90e.chunk.css"
+  },
+  {
+    "revision": "bc151d9f7783a5b0cec4",
+    "url": "/waysamurai/static/js/2.cecdfb8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/waysamurai/static/js/2.cecdfb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d6e623bf248a05b00f",
+    "url": "/waysamurai/static/js/main.252d8109.chunk.js"
+  },
+  {
+    "revision": "f80215cc93238419d2e9",
+    "url": "/waysamurai/static/js/runtime-main.0bbefa4d.js"
+  }
+]);
